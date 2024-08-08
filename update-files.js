b50a4a8931372d7a27bf7b0e99358d818d90ac5e
Daniel Rosselló <daniel.rossello@secuoyas.com>
2024-08-08 15:55:27 +0200

    ci: Update API handling and add dotenv support
    Refactor API functions and introduce dotenv for environment variable management. Updated .gitignore to include .env files and added .env.example for reference. Adjusted update-files.js script to utilize API_URL from environment variables.

--- update-files.js
+++ update-files.js
@@ -1,11 +1,22 @@
 import fs from 'fs';
 import path from 'path';
 
+import { config } from 'dotenv';
+
+config();
+
 const newVersion = process.argv[2];
 if (!newVersion) {
   console.error('Por favor, proporciona una nueva versión como argumento');
   process.exit(1);
 }
+
+const API_URL = process.env.API_URL;
+if (!API_URL) {
+  console.error('API_URL no está definida en las variables de entorno');
+  process.exit(1);
+}
+
 
 const updateFile = (filePath, regex, replacement) => {
   const fullPath = path.join(process.cwd(), filePath);
@@ -21,3 +32,7 @@ updateFile('public/manifest.json', /"version":\s*"\d+\.\d+\.\d+"/, `"version": "
 updateFile('src/ui/components/Footer/index.tsx', /v\d+\.\d+\.\d+/, `v${newVersion}`);
 
 console.log(`Actualizados los archivos a la versión ${newVersion}`);
+
+updateFile('share/api.ts', /v\d+\.\d+\.\d+/, `v${newVersion}`);
+// Actualizar API_URL en el archivo correspondiente
+updateFile('share/api.ts', /export const API_URL = ".*";/, `export const API_URL = "${API_URL}";`);
