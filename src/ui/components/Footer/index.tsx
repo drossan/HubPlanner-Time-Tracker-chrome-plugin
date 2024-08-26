3d443bc482c052613066940a20ee59bb882029d3
Daniel Rosselló <daniel.rossello@secuoyas.com>
2024-08-26 18:13:12 +0200

    chore: release v1.8.0

--- src/ui/components/Footer/index.tsx
+++ src/ui/components/Footer/index.tsx
@@ -9,8 +9,8 @@ const Footer = () => {
 			>
 				Hub Planner
 			</a>
-			<a href="https://github.com/Secuoyas-Experience/HubPlanner-Time-Tracker-Chrome-Extension/releases/tag/v1.7.3"
-			   target="_blank">v1.7.3</a>
+			<a href="https://github.com/Secuoyas-Experience/HubPlanner-Time-Tracker-Chrome-Extension/releases/tag/v1.8.0"
+			   target="_blank">v1.8.0</a>
 		</section>
 	);
 };
